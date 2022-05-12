eeee86ed071709dd26fd6e7c6f537beb00629cf3
fidele <randriambahoakafidele7@gmail.com>
2022-05-12 11:01:02 +0300

    premier

---
+++ src/main/java/com/inclusiv/cdan3/pharmacheck/PharmacheckApplication.java
@@ -0,0 +1,13 @@
+package com.inclusiv.cdan3.pharmacheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacheckApplication.class, args);
+	}
+
+}
